fb69e19fce0287700d9e879ca5f97ed780a70a8a
sergioia-dev <sergioidarraga2110@gmail.com>
2026-09-20 21:16:16 -0500

    Update

--- flake.nix
+++ flake.nix
@@ -24,6 +24,7 @@
       # to regenerate extensions/package.json + extensions/package-lock.json.
       npmExtensionSpecs = [
         # "pi-vim@0.14.2"
+        "@narumitw/pi-usage@0.60.10"
         "pi-nvim@0.2.5"
         "pi-catppuccin-tui@0.1.3"
         "@nguyenquangthai/pi-todo@0.6.3"
